52162e0872a4c77b407f37b4ce1ef3264d8ddb19
pachax001 <93702860+pachax001@users.noreply.github.com>
2023-09-24 13:53:23 +0530

    Update upload.py

--- helpers/upload.py
+++ helpers/upload.py
@@ -13,6 +13,7 @@ from helpers.progress import progress_func
 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import Config
+from plugins.extractor import user_id, user_first_name
 log_channel = Config.LOG_CHANNEL
 bot_username = Config.BOT_USERNAME
 async def upload_audio(client, message, file_loc):

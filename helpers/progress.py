b3d9c8d82e3c86c3d0a1dac9cb2948ba89c046ae
pachax001 <93702860+pachax001@users.noreply.github.com>
2024-05-18 07:15:58 +0530

    Update progress.py

--- helpers/progress.py
+++ helpers/progress.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 # @trojanzhex
 
-
 import os
 import time
-
+import asyncio
 
 PRGRS = {}
 
@@ -21,33 +20,31 @@ async def progress_func(
     if round(diff % 5.00) == 0 or current == total:
         percentage = current * 100 / total
         speed = current / diff
-        elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
+        elapsed_time = round(diff * 1000)  # milliseconds
+        time_to_completion = round((total - current) / speed * 1000)  # milliseconds
         estimated_total_time = elapsed_time + time_to_completion
 
-        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+        elapsed_time_str = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time_str = TimeFormatter(milliseconds=estimated_total_time)
 
         PRGRS[f"{message.chat.id}_{message.id}"] = {
             "current": humanbytes(current),
             "total": humanbytes(total),
             "speed": humanbytes(speed),
             "progress": percentage,
-            "eta": elapsed_time
+            "eta": elapsed_time_str
         }
-
 
 def humanbytes(size):
     if not size:
         return ""
-    power = 2**10
+    power = 2 ** 10
     n = 0
-    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
+    Dic_powerN = {0: '', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
     while size > power:
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
-
 
 def TimeFormatter(milliseconds: int) -> str:
     seconds, milliseconds = divmod(int(milliseconds), 1000)
@@ -59,4 +56,4 @@ def TimeFormatter(milliseconds: int) -> str:
         ((str(minutes) + "m, ") if minutes else "") + \
         ((str(seconds) + "s, ") if seconds else "") + \
         ((str(milliseconds) + "ms, ") if milliseconds else "")
-    return tmp[:-2]
+    return tmp.rstrip(', ')

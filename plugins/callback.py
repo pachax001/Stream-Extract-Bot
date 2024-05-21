dcea291cafcbd86ad0c9e70a6e89c5900fb8340d
pachax001 <pachax001@gmail.com>
2024-05-21 14:51:46 +0530

    Silly

--- plugins/callback.py
+++ plugins/callback.py
@@ -75,7 +75,7 @@ async def cb_handler(client, query):
 
     elif query.data == "progress_msg":
         try:
-            msg = "Progress Details...\n\nCompleted : {current}\nTotal Size : {total}\nSpeed : {speed}\nProgress : {progress:.2f}%\nElapsed Time : {elapsed}%\nETA: {eta}"
+            msg = "Progress Details...\n\nCompleted : {current}\nTotal Size : {total}\nSpeed : {speed}\nProgress : {progress:.2f}%\nElapsed Time : {elapsed}\nETA: {eta}"
             await query.answer(
                 msg.format(
                     **PRGRS[f"{query.message.chat.id}_{query.message.id}"]

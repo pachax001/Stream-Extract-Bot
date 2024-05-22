9ffd88096e16ebdd6209f95b4d6386f427065204
pachax001 <pachax001@gmail.com>
2024-05-22 10:18:27 +0530

    Retry if file is downloaded partially

--- helpers/download.py
+++ helpers/download.py
@@ -163,6 +163,7 @@ async def download_file(client, message):
                         f"**Select the Stream to be Extracted for {file_name}**",
                         reply_markup=InlineKeyboardMarkup(buttons)
                         )
+                    break
         except Exception as e:
             logger.error(f"Error while downloading {file_name}: {e}")
             await msg.edit_text(f"Error while downloading {file_name}")
